7b6c1d2a0d20db5dc697e581c76f9aeba1d115bd
ILLI1979 <ILLI1979@yandex.ru>
2022-04-25 09:13:25 +0300

    Тестовые задания

---
+++ src/test/task5/Safe.java
@@ -0,0 +1,14 @@
+package test.task5;
+
+public class Safe {
+    private final int vol;
+
+    Safe (int vol) {
+        this.vol = vol;
+    }
+    public int getVol() {
+        return vol;
+    }
+
+}
+
